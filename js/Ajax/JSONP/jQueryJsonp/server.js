8cbef5719a1db0db2c21edf9e2703ceb4fecb635
7986LiChang <798602874@qq.com>
2017-08-17 12:47:04 +0800

    jsonp

---
+++ js/Ajax/JSONP/jQueryJsonp/server.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var urllib = require('url');
+
+var data = {'name':'lichang', 'addr':'chengdu'};
+
+http.createServer(function(requset, response){
+	response.writeHead(200, {'Content-type':'text/plain'});
+
+	var params = urllib.parse(requset.url, true);
+
+	if(params.query && params.query.callback){
+		var str = params.query.callback + '(' + JSON.stringfy(data) + ')';  //jsonp
+		response.end(str);  //将str发回客户端
+	}
+	else{
+		response.end(JSON.stringfy(data));   //普通json
+	}
+}).listen(1234);
+
+console.log("Server is running at http://127.0.0.1:1234");
